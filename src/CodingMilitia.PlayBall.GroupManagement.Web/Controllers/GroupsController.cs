fdf9be5ee1d5eff96ee340152606f83e8b952016
aminov <alami@tshtt.uz>
2021-09-22 14:43:07 +0500

    Model add in Controller

--- src/CodingMilitia.PlayBall.GroupManagement.Web/Controllers/GroupsController.cs
+++ src/CodingMilitia.PlayBall.GroupManagement.Web/Controllers/GroupsController.cs
@@ -1,15 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using CodingMilitia.PlayBall.GroupManagement.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CodingMilitia.PlayBall.GroupManagement.Web.Controllers
 {
     [Route("groups")] 
     public class GroupsController : Controller
     {
+
+        private static long currentGroupId;
+        private static List<GroupViewModel> groups = new List<GroupViewModel>
+        {
+            new GroupViewModel { Id = 1, Name = "Sample Group N1" },
+            new GroupViewModel { Id = 1, Name = "Sample Group N2" }
+        };
+        
         [HttpGet]
         [Route("")] //not needed because Index would be used as default anyway
         public IActionResult Index()
         {
-            return View();
+            return View(groups);
         }
     }
 }
---
+++ src/CodingMilitia.PlayBall.GroupManagement.Web/Models/GroupViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CodingMilitia.PlayBall.GroupManagement.Web.Models
+{
+    public class GroupViewModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
