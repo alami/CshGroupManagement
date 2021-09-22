467be48a16d1e57c087bd07db607b97f9f9a7f4f
aminov <alami@tshtt.uz>
2021-09-22 11:11:26 +0500

    Startup..Controller..Err|services.AddMvc(option => option.EnableEndpointRouting = false);

---
+++ src/CodingMilitia.PlayBall.GroupManagement.Web/Controllers/GroupsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodingMilitia.PlayBall.GroupManagement.Web.Controllers
+{
+    [Route("groups")] 
+    public class GroupsController : Controller
+    {
+        [HttpGet]
+        [Route("")] //not needed because Index would be used as default anyway
+        public IActionResult Index()
+        {
+            return Content("groups");
+        }
+    }
+}
