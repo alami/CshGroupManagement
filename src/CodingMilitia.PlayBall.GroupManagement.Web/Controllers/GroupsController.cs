908d2145deb701d8fa5482a913c94fda333c2954
aminov <alami@tshtt.uz>
2021-09-22 15:58:56 +0500

    Details action

--- src/CodingMilitia.PlayBall.GroupManagement.Web/Controllers/GroupsController.cs
+++ src/CodingMilitia.PlayBall.GroupManagement.Web/Controllers/GroupsController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using CodingMilitia.PlayBall.GroupManagement.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,11 +9,11 @@ namespace CodingMilitia.PlayBall.GroupManagement.Web.Controllers
     public class GroupsController : Controller
     {
 
-        private static long currentGroupId;
+        private static long currentGroupId = 1;
         private static List<GroupViewModel> groups = new List<GroupViewModel>
         {
             new GroupViewModel { Id = 1, Name = "Sample Group N1" },
-            new GroupViewModel { Id = 1, Name = "Sample Group N2" }
+            new GroupViewModel { Id = 2, Name = "Sample Group N2" },
         };
         
         [HttpGet]
@@ -20,6 +21,18 @@ namespace CodingMilitia.PlayBall.GroupManagement.Web.Controllers
         public IActionResult Index()
         {
             return View(groups);
+        }
+        
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult Details(long id)
+        {
+            var group = groups.SingleOrDefault(g => g.Id == id);
+            if (group == null)
+            {
+                return NotFound();
+            }
+            return View(group);
         }
     }
 }
