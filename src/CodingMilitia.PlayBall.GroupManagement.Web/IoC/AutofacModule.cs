b3540bc6a773d9f80462da56c9374344c7e3f50b
aminov <alami@tshtt.uz>
2021-09-24 17:36:27 +0500

    ch05 DI with other container 'Autofac'

---
+++ src/CodingMilitia.PlayBall.GroupManagement.Web/IoC/AutofacModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using CodingMilitia.PlayBall.GroupManagement.Business.Impl.Services;
+using CodingMilitia.PlayBall.GroupManagement.Business.Services;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public class AutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<InMemoryGroupsService>().As<IGroupsService>().SingleInstance(); 
+        }
+    }
+}
--- src/CodingMilitia.PlayBall.GroupManagement.Web/Startup.cs
+++ src/CodingMilitia.PlayBall.GroupManagement.Web/Startup.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,10 +12,19 @@ namespace CodingMilitia.PlayBall.GroupManagement.Web
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddMvc(option => option.EnableEndpointRouting = false);
-            services.AddBusiness();
+            //если использовать DI контейнер поумолчанию, раскомментировать
+            //services.AddBusiness();
+            
+            //--add autofac
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterModule<AutofacModule>();
+            containerBuilder.Populate(services);
+            var container = containerBuilder.Build();
+            return new AutofacServiceProvider(container);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
