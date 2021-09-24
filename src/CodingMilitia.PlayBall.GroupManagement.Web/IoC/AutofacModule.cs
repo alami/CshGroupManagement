49a92aa07ccf32ffa7ac025bb68628f00c640dd2
aminov <alami@tshtt.uz>
2021-09-24 17:55:40 +0500

    ch05 DI decorator

--- src/CodingMilitia.PlayBall.GroupManagement.Web/IoC/AutofacModule.cs
+++ src/CodingMilitia.PlayBall.GroupManagement.Web/IoC/AutofacModule.cs
@@ -1,5 +1,8 @@
-﻿using Autofac;
+﻿using System;
+using System.Collections.Generic;
+using Autofac;
 using CodingMilitia.PlayBall.GroupManagement.Business.Impl.Services;
+using CodingMilitia.PlayBall.GroupManagement.Business.Models;
 using CodingMilitia.PlayBall.GroupManagement.Business.Services;
 
 namespace Microsoft.Extensions.DependencyInjection
@@ -8,7 +11,41 @@ namespace Microsoft.Extensions.DependencyInjection
     {
         protected override void Load(ContainerBuilder builder)
         {
-            builder.RegisterType<InMemoryGroupsService>().As<IGroupsService>().SingleInstance(); 
+            builder.RegisterType<InMemoryGroupsService>().Named<IGroupsService>("groupService").SingleInstance();
+            builder.RegisterDecorator<IGroupsService>((context, service)
+                =>new GroupsServiceDecorator(service),"groupService");
+        }
+        private class GroupsServiceDecorator : IGroupsService
+        {
+            private readonly IGroupsService _inner;
+
+            public GroupsServiceDecorator(IGroupsService inner)
+            {
+                _inner = inner;
+            }
+            public IReadOnlyCollection<Group> GetAll()
+            {
+                Console.WriteLine($"######## Helooo from {nameof(GetAll)} #########");
+                return _inner.GetAll() ;
+            }
+
+            public Group GetById(long id)
+            {
+                Console.WriteLine($"######## Helooo from {nameof(GetById)} #########");
+                return _inner.GetById(id) ;
+            }
+
+            public Group Update(Group group)
+            {
+                Console.WriteLine($"######## Helooo from {nameof(Update)} #########");
+                return _inner.Update(group) ;
+            }
+
+            public Group Add(Group group)
+            {
+                Console.WriteLine($"######## Helooo from {nameof(Add)} #########");
+                return _inner.Add(group) ;
+             }
         }
     }
 }
